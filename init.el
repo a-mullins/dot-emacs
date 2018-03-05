b71523f593a22a20326ef68dcced2388d36001df
Adam Mullins <adam.l.m@gmail.com>
2018-03-05 18:13:14 -0500

    remove marmalade package archive

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-	("marmalade" . "https://marmalade-repo.org/packages/")
+	;;("marmalade" . "https://marmalade-repo.org/packages/")
 	("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
