fdcfe75d39d4742f3aee8a1ac3c64c2d25fb423d
Adam Mullins <adam.l.m@gmail.com>
2019-06-09 04:10:15 -0400

    cruft removal

--- init.el
+++ init.el
@@ -1,96 +1,76 @@
 ;; init.el -- emacs config
 
-;; --------------------------------------------------------------------------
-;; I/O
-;;
-;; Switch to messages buffer to communicate with user during setup, &c
-;; --------------------------------------------------------------------------
-
-;; TODO: pop up temporary frame & switch to temporary buffer only if
-;; TODO: diagnostic / status messages need to be displayed.
-(setq initial-buffer-choice
-      (lambda () (get-buffer "*Messages*")))
-
-;; --------------------------------------------------------------------------
+;; -------------------------------------------------------------------
 ;; PACKAGES
-;;
-;; Bootstrap package manager.
-;; --------------------------------------------------------------------------
-
+;; -------------------------------------------------------------------
 (require 'package)
-
 (setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-	;;("marmalade" . "https://marmalade-repo.org/packages/")
-	("melpa-stable" . "https://stable.melpa.org/packages/")))
-
+      '(
+	("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa-stable" . "https://stable.melpa.org/packages/")
+	;; ("melpa" . "https://melpa.org/packages/")
+	;; ("marmalade" . "https://marmalade-repo.org/packages/")
+	))
 (package-initialize)
+;; Archive listings and installed packages should now be available.
 
-(unless package-archive-contents
-  (message "Fetching package index. This should only take a few moments.")
-  (package-refresh-contents))
-
-;; Fetch and install use-package, if it isn't already installed.
+;; If use-package is missing, prompt to download now.
 (unless (package-installed-p 'use-package)
-  (message "Downloading and installing package 'use-package'.")
-  (package-install 'use-package))
+  (when (yes-or-no-p "There are missing packages. Fetch them now? ")
+    (package-refresh-contents)
+    (package-install 'use-package)))
 
-;; use-package usage:
-;; (use-package foo
-;;   ;; evaluated BEFORE package is loaded
-;;   :init
-;;   ;; evaluated AFTER package is loaded
-;;   :config
-;;   ;; if not available, install with package manager
-;;   :ensure t
-;;   )
+(when (package-installed-p 'use-package)
+  (use-package elpy
+    :ensure t
+    :config (elpy-enable))
 
-(use-package elpy
-  :ensure t
-  :config (elpy-enable))
+  (use-package solarized-theme
+    :ensure t
+    :config (load-theme 'solarized-dark t))
 
-(message (concat "\n"
-		 "!!!   IF WORKING ON A PYTHON PROJECT    !!!\n"
-                 "!!!  ENSURE THAT ALL ELPY HELPERS ARE   !!!\n"
-		 "!!! INSTALLED BY RUNNING: 'elpy-config' !!!\n"))
+  (use-package paredit
+    :ensure t
+    :init
+      (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+      (add-hook 'lisp-mode-hook 'paredit-mode))
 
-(use-package solarized-theme
-  :ensure t
-  :config (load-theme 'solarized-dark t))
-
-(use-package paredit
-  :ensure t
-  :config (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
-
-;; (use-package flycheck
-;;   :ensure t
-;;   :init (global-flycheck-mode))
-
-;; (use-package haskell-mode
-;;   :ensure t)
+  (use-package markdown-mode
+    :ensure t
+    :commands (markdown-mode gfm-mode)
+    :mode (("README\\.md\\'" . gfm-mode)
+           ("\\.md\\'" . markdown-mode)
+           ("\\.markdown\\'" . markdown-mode))
+    :init (setq markdown-command "multimarkdown"))
+) ;; END when (package-installed-p 'use-package)
 
 ;; -------------------------------------------------------------------
 ;; UI / VISUAL
 ;; -------------------------------------------------------------------
 
-;; set font & size according to which system.
-(setq init-msg-new-machine
-      "using default face, running on unknown machine named: ")
-(pcase (system-name)
-  ('"denali"
-   (set-face-attribute 'default nil :family "inconsolata" :height 130))
-  ('"Air.local"
-   (set-face-attribute 'default nil :family "inconsolata" :height 180))
-  ;; default case
-  (_ (message (concat init-msg-new-machine (system-name)))
-     (set-face-attribute 'default nil :family "inconsolata" :height 120)))
+;; set font & size according to which system emacs is running on.
+(apply 'set-face-attribute 'default nil
+  (pcase (system-name)
+    ("denali"    '(:family "inconsolata" :height 130))
+    ("Air.local" '(:family "inconsolata" :height 180))
+    (_           '(:family "inconsolata" :height 120))))
 
-;;(tool-bar-mode -1)
-
-;; ;; Hooks.
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'prog-mode-hook 'column-number-mode)
-(add-hook 'prog-mode-hook 'toggle-truncate-lines)
-(add-hook 'prog-mode-hook 'show-paren-mode)
-
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (setq show-paren-delay 0)
+
+
+;; HOOKs for prog-mode. This covers python, lisp, etc etc.
+(mapc
+ (lambda (hook) (add-hook 'prog-mode-hook hook))
+   '(
+     linum-mode
+     column-number-mode
+     (lambda () (toggle-truncate-lines 1))
+     show-paren-mode
+     ))
+
+;; dired mode
+(setq dired-listing-switches "-aDFhl")
+(add-hook 'dired-mode-hook
+	 (lambda () (toggle-truncate-lines 1)))
