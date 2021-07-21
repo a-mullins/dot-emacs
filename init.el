5e0b812e31dfb8ccc2a5ab78b62777e8db7a757c
Adam Mullins <adam.l.m@gmail.com>
2021-07-21 14:48:31 -0400

    minor formatting

--- init.el
+++ init.el
@@ -29,9 +29,9 @@
 
 (apply 'set-face-attribute 'default nil
        (pcase (system-name)
-         ("denali"    '(:family "inconsolata" :height 130))
-         ("Air.local" '(:family "monaco" :height 160))
-         (_           '(:family "inconsolata" :height 120))))
+         ("denali"    '(:family "inconsolata" :height 140))
+         ("deimos   " '(:family "monaco" :height 160))
+         (_           '(:family "inconsolata" :height 130))))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -55,22 +55,18 @@
 
 ;; If use-package is missing, prompt to download now.
 (unless (package-installed-p 'use-package)
-  (when (yes-or-no-p "There are missing packages. Fetch them now? ")
+  (when (yes-or-no-p "`use-package` is missing. Fetch it now? ")
     (package-refresh-contents)
     (package-install 'use-package)))
 
+;; Packages are sorted by approximate frequency of use.
 (when (package-installed-p 'use-package)
-  (use-package elpy
+  ;; (use-package solarized-theme
+  ;;   :ensure t
+  ;;   :config (load-theme 'solarized-dark t))
+  (use-package subatomic-theme
     :ensure t
-    :defer t
-    :init
-    (advice-add 'python-mode :before 'elpy-enable)
-    (setq python-shell-interpreter "python3"
-          python-shell-interpreter-args "-i"))
-
-  (use-package solarized-theme
-    :ensure t
-    :config (load-theme 'solarized-dark t))
+    :config (load-theme 'subatomic t))
 
   (use-package paredit
     :ensure t
@@ -78,20 +74,43 @@
     (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
     (add-hook 'lisp-mode-hook 'paredit-mode))
 
-  (use-package markdown-mode
+  (use-package elpy
     :ensure t
-    :commands (markdown-mode gfm-mode)
-    :mode (("README\\.md\\'" . gfm-mode)
-           ("\\.md\\'" . markdown-mode)
-           ("\\.markdown\\'" . markdown-mode))
-    :init (setq markdown-command "multimarkdown"))
+    :defer t
+    :init
+    (advice-add 'python-mode :before 'elpy-enable)
+    (setq
+          python-shell-interpreter "python3"
+          python-shell-interpreter-args "-i"
+          elpy-rpc-python-command "python3")
+    ;;(add-to-list 'exec-path (expand-file-name "~/.local/bin"))
+    )
 
-  (use-package slime
-    :mode ("\\.cl\\'" . common-lisp-mode)
-    :config
-    (add-to-list 'slime-contribs 'slime-repl)
-    (setq inferior-lisp-program "/usr/bin/clisp"))
-  ) ;; END when (package-installed-p 'use-package)
+  ;; TODO: only turn on when window size is > 80 col?
+  ;; (use-package fill-column-indicator
+  ;;   :ensure t
+  ;;   :init
+  ;;   (setq fci-rule-column 80)
+  ;;   :config
+  ;;   (add-hook 'prog-mode-hook
+  ;;             (lambda () (fci-mode 1))
+  ;;             ))
+
+  ;; (use-package markdown-mode
+  ;;   :ensure t
+  ;;   :commands (markdown-mode gfm-mode)
+  ;;   :mode (("README\\.md\\'" . gfm-mode)
+  ;;          ("\\.md\\'" . markdown-mode)
+  ;;          ("\\.markdown\\'" . markdown-mode))
+  ;;   :init (setq markdown-command "multimarkdown"))
+
+  ;; (use-package slime
+  ;;   :mode ("\\.cl\\'" . common-lisp-mode)
+  ;;   :config
+  ;;   (add-to-list 'slime-contribs 'slime-repl)
+  ;;   (setq inferior-lisp-program "/usr/bin/clisp"))
+
+) ;; END when (package-installed-p 'use-package)
 
 
 ;; -------------------------------------------------------------------
