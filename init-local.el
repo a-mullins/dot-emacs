3296fb0c2a69d7869a0daf4cb92b706db5427b4c
Adam Mullins <adam.l.m@gmail.com>
2025-12-17 07:56:31 -0500

    add locals for macbook

--- init-local.el
+++ init-local.el
@@ -5,5 +5,16 @@
     (add-to-list 'default-frame-alist frame-param))
   (set-face-attribute 'default nil :font "IBM Plex Mono" :height 120))
 
+(defun shasta-locals ()
+  (dolist
+      (frame-param '((width . 80)
+                     (height . 41)))
+    (add-to-list 'default-frame-alist frame-param)
+    (remove-hook 'prog-mode-hook #'display-line-numbers-mode))
+  (set-face-attribute 'default nil :font "Menlo" :height 150))
+
+(frame-parameter nil 'width)
+
 (pcase (downcase (car (split-string (system-name) "\\.")))
-  ("denali" (denali-locals)))
+  ("denali" (denali-locals))
+  ("shasta" (shasta-locals)))
